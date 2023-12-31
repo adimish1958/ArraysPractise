b0700adb728f88fba9ed823e30d2d044e15cb8ee
adimish1958 <51970475+adimish1958@users.noreply.github.com>
2023-12-31 14:01:12 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,20 @@
+public static int sqrt(int n)
+    {
+        if (n < 2) {
+            return n;
+        }
+        int low = 1, high = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (mid * mid == n) {
+                return mid;
+            }
+            else if (mid * mid < n) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return high;
+    }
