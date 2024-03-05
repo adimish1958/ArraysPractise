f617aa123870eb17b33b21d85e4f78ff23595c12
adimish1958 <51970475+adimish1958@users.noreply.github.com>
2024-03-05 15:00:42 +0530

    Create findpivotindexormiddleindex.java

---
+++ findpivotindexormiddleindex.java
@@ -0,0 +1,104 @@
+/*
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+Example 3:
+
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-1000 <= nums[i] <= 1000
+
+Note:  ALso same solution for middle index of an array
+
+
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
+Example 2:
+
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
+Example 3:
+
+Input: nums = [2,5]
+Output: -1
+Explanation: There is no valid middleIndex.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-1000 <= nums[i] <= 1000
+ */
+
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int pivot=0;
+        int index=-1;
+        int leftsum=0;
+        int rightsum=0;
+        for(int i=0;i<nums.length;i++){
+            rightsum+=nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            pivot=nums[i];
+            
+            rightsum=rightsum-pivot;
+            if(leftsum==rightsum){
+                index=i;
+                break;
+            }
+            else{
+                leftsum+=pivot;
+            }
+        }
+        return index;
+    }
+}
